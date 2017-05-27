e6b608c39970fdaee57918dc1a62810fba5cecc1
Bill Zhang <zzlfullstack@gmail.com>
2017-05-27 17:31:23 +0800

    搭建了轮播的基本结构
    计划做一个通栏banner，使浏览器窗口缩放的时候banner始终居中显示

---
+++ src/js/script.js
@@ -0,0 +1,8 @@
+// $(document).ready(function() {
+// 	var slide = $('.slide-li');
+// 	console.log(slide);
+// });
+window.onload = function() {
+	var slide = document.getElementsByClassName('slide-li');
+	console.log(slide);
+}
