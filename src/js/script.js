698a917215c2371454f737555bb76b0dfe12cbd4
seed-fe <zzlfullstack@gmail.com>
2017-05-30 23:08:57 +0800

    动态生成了用于无缝滚动的节点

--- src/js/script.js
+++ src/js/script.js
@@ -2,7 +2,17 @@
 // 	var slide = $('.slide-li');
 // 	console.log(slide);
 // });
-window.onload = function() {
-	var slide = document.getElementsByClassName('slide-li');
-	console.log(slide);
-}
+$(document).ready(function() {
+    $('.slide-li').eq(0).clone().addClass('clone').prependTo('#slides');
+    $('.slide-li:last-child').clone().addClass('clone').appendTo('#slides');
+    var slideNum = $('.slide-li').length,
+        width = 1920 * slideNum;
+        // windowWidth = window.innerWidth;
+    $('#slides').css('width', width + 'px');
+    // $('.slide-li').css('width', windowWidth + 'px');
+    $(window).on('resize', function() {
+        // windowWidth = window.innerWidth;
+        // $('.slide-li').css('width', windowWidth + 'px');
+    });
+    $('.next')
+});
