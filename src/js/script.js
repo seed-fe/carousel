d6d048304f1e0bcf43c883bd1435720d438cd3a8
Bill Zhang <zzlfullstack@gmail.com>
2017-05-31 13:53:09 +0800

    完成了箭头切换的功能，准备封装成jQuery插件
    制作无缝滚动效果时要注意，先完成animate方法，滚动到附属图，然后在animate方法的complete回调函数里对slides进行复位

--- src/js/script.js
+++ src/js/script.js
@@ -3,16 +3,46 @@
 // 	console.log(slide);
 // });
 $(document).ready(function() {
-    $('.slide-li').eq(0).clone().addClass('clone').prependTo('#slides');
-    $('.slide-li:last-child').clone().addClass('clone').appendTo('#slides');
+    $('.slide-li:last-child').clone().addClass('clone').prependTo('#slides');
+    $('.slide-li').eq(1).clone().addClass('clone').appendTo('#slides');
+    
     var slideNum = $('.slide-li').length,
-        width = 1920 * slideNum;
-        // windowWidth = window.innerWidth;
-    $('#slides').css('width', width + 'px');
-    // $('.slide-li').css('width', windowWidth + 'px');
-    $(window).on('resize', function() {
-        // windowWidth = window.innerWidth;
-        // $('.slide-li').css('width', windowWidth + 'px');
+        width = 1920 * slideNum,
+        left = -1920,
+        index = 1,
+        windowWidth = window.innerWidth,
+        autoTimer = null;
+    var imgWidth = $('.slide-li').width();
+    $('.slide-li img').css('marginLeft', -(imgWidth - windowWidth) / 2 + 'px');
+    $('#slides').css({'width': width + 'px', 'left': left + 'px'});
+    // 左右箭头切换
+    $('.next').on('click', function(e) {
+        left-=1920;
+        $('#slides').animate({left: left + 'px'}, 800, function() {
+            index += 1;
+            if (index > slideNum - 2) {
+                // 这里必须把css的left值复位的语句放在animate方法的complete回调函数里，否则由于异步性，会先执行-1920px的语句，然后800ms后执行完毕animate方法，又把left值改为-9600px，无法实现无缝滚动
+                $('#slides').css('left', -1920 + 'px');
+                index = 1;
+                left = -1920;
+            }
+        });
     });
-    $('.next')
+    $('.prev').on('click', function(e) {
+        left += 1920;
+        $('#slides').animate({left: left + 'px'}, 800, function() {
+            index -= 1;
+            if (index < 1) {
+                // 这里必须把css的left值复位的语句放在animate方法的complete回调函数里，否则由于异步性，会先执行-1920px的语句，然后800ms后执行完毕animate方法，又把left值改为-9600px，无法实现无缝滚动
+                $('#slides').css('left', -1920*4 + 'px');
+                index = 4;
+                left = -1920*4;
+            }
+        })
+        
+    });
+    $(window).on('resize', function() {
+        windowWidth = window.innerWidth;
+        $('.slide-li img').css('marginLeft', -(imgWidth - windowWidth) / 2 + 'px');
+    });
 });
