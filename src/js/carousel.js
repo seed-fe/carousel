29a89cd54ab05bb5ee74bda6184ff543ee3d0dbc
Bill Zhang <zzlfullstack@gmail.com>
2017-05-31 17:40:35 +0800

    添加了底部按钮切换和自动切换的功能
    * 代码需进行封装优化
    * 可考虑封装成jQuery插件

---
+++ src/js/carousel.js
@@ -0,0 +1,121 @@
+// 为避免$别名与其他库发生冲突，可以使用jQuery， 或者在立即调用的函数表达式（ IIFE）中传入$，使其成为一个局部变量，这么做也可以防止污染全局命名空间。
+;(function($, window, document, undefined) { // 将window/document等系统变量作为参数传递到插件内部，这样在需要访问这些系统变量的时候就可以访问这些参数，也就是访问局部变量，比访问全局变量速度更快，有少许性能提升
+	/*用面向对象的方式重写，然后封装成jquery插件*/
+	function Slide (el, opts) {
+		// body...
+		this.opts = $.extend({}, Slide.DEFAULTS, opts);
+	    this.$el = $(el);
+	    this.tabs = this.$el.find('.' + this.opts.tabClass);
+	    var tabs = this.tabs;
+	    this.contents = this.$el.find('.' + this.opts.contentClass);
+	    var contents = this.contents;
+	    var trigger = this.opts.trigger;
+	    var that = this;
+	    // 设定默认显示第几个tab
+	    if (this.opts.invoke > 1) {
+	    	this._tabSwitch(this.opts.invoke - 1);
+	    }
+	    // 控制自动切换
+	    if (this.opts.autoPlay === true) {
+	    	if (this.timerAuto) {
+				clearInterval(this.timerAuto);
+				this.timerAuto = null;
+			}
+	    	this._autoPlay();
+	    	// 鼠标移入tab区域停止自动切换，移出恢复
+	    	this.$el.hover(function(event) {
+				/* Act on the event */
+				clearInterval(that.timerAuto);
+				that.timerAuto = null;
+			}, function(event) {
+					event.preventDefault();
+					/* Act on the event */
+					that._autoPlay();
+				}
+	    	);
+	    }
+	    // 切换触发方式
+	    if (trigger === 'click') {
+	    	tabs.on(trigger, function(e) {
+	    		/* Act on the event */
+	    		e.preventDefault();
+	    		that.curIndex = tabs.index($(this));
+	    		that._tabSwitch(that.curIndex);
+	    		
+	    	});
+	    }
+	    if (trigger === 'mouseenter') {
+	    	tabs.on(trigger, function(e) {
+	    		/* Act on the event */
+	    		that.curIndex = tabs.index($(this));
+	    		if (that.timerDelay) {
+					clearTimeout(that.timerDelay);
+					that.timerDelay = null;
+				}
+				that.timerDelay = setTimeout(function() {
+					that._tabSwitch(that.curIndex);
+					that.timerDelay = null; // debounce去抖，保证清除动画队列
+				}, 500);
+	    	});
+	    }
+	}
+	Slide.prototype = {
+		// body...
+		constructor: Tab,
+		autoIndex: 0, // 用来记录自动切换的索引
+		curIndex: 0, // 用来记录当前切换的tab的索引
+		timerAuto: null, // 自动切换定时器
+		timerDelay: null, // 延迟切换定时器
+		_tabSwitch: function(pIndex) {
+			// 硬切换
+			if (this.opts.mode === 'none') {
+				this.tabs.eq(pIndex).addClass(this.opts.tabClassCur).siblings().removeClass(this.opts.tabClassCur);
+				this.contents.eq(pIndex).addClass(this.opts.contentClassCur).siblings().removeClass(this.opts.contentClassCur);
+			}
+			// 淡入淡出切换
+			if (this.opts.mode === 'fade') {
+				this.tabs.eq(pIndex).addClass(this.opts.tabClassCur).siblings().removeClass(this.opts.tabClassCur);
+				this.contents.eq(pIndex).fadeIn().siblings().fadeOut();
+			}
+			// 鼠标移入触发切换后要改变自动切换的index值，让下次自动切换的时候从当前tab开始切换
+			if (this.opts.autoPlay === true) {
+	    		this.autoIndex = pIndex;
+	    	}
+		},
+		_autoPlay: function() {
+			var tabs = this.tabs;
+			var that = this; // 在原型方法内部把this负值给that，而不是在原型属性里，如果在原型属性里赋值而在外层函数里没有把this负值给that，在定时器内部还是访问不到的，因为闭包里的this还是指向全局作用域（这里用到了定时器，定时器内this指向window对象），而在外层函数里把this负值给that，闭包中的匿名函数就能访问到了；这里的this在调用时就是Tab的实例
+			this.timerAuto = setInterval(function() {
+	    		that.autoIndex++;
+				// 索引达到最大时要重新归零
+				if (that.autoIndex >= tabs.length) {
+					that.autoIndex = 0;
+				}
+	    		that._tabSwitch(that.autoIndex);
+	    	}, 2000);
+		}
+	}
+	Slide.DEFAULTS = {
+		tabClass: 'tab', // tab标签类名，可自定义
+		tabClassCur: 'tab-selected', // 标签选定时添加的类名，在该类名下添加标签选定状态的样式
+		contentClass: 'content', // content标签类名，可自定义
+		contentClassCur: 'content-current', // 内容展示时添加的类名，在该类名下添加内容展示时的样式
+		
+		mode: 'slide', // 切换时的动画效果，默认是slide，滑动；可选fade，淡入淡出
+		autoPlay: true, // 是否自动切换，默认true
+		arrow: true, // 是否显示切换箭头
+		button: true, // 是否显示底部切换按钮
+		trigger: 'mouseenter', // banner下方按钮切换触发方式，默认mouseenter，可选click
+		// invoke: 1 // 初始默认显示第几个tab
+	}
+	// 将一个对象添加到jquery的原型上从而提供新的jquery实例方法，这里就相当于给jquery实例对象提供了一个backtop方法，这也是实现jquery插件的常用方法，对象里定义的方法名就是插件方法名
+	$.fn.extend({
+	    slide: function(opts) {
+	        // return 是为了实现连缀，这里的this指代调用插件时用jquery选择的元素（转化成了一个jQuery对象的实例），调用each方法是因为可能会选择多个元素
+	        // .each()方法的回调函数的执行环境是当前dom元素，也就是说this始终指向当前dom元素，所以下面的回掉函数就是创建一个BackTop的实例，并把当前dom元素作为参数传入
+	        return this.each(function() {
+	            new Slide(this, opts); // 这里的this指向dom元素
+	        })
+	    }
+	})
+})(jQuery, window, document);
--- src/js/script.js
+++ src/js/script.js
@@ -3,43 +3,107 @@
 // 	console.log(slide);
 // });
 $(document).ready(function() {
+    // 插入用于无缝滚动的附属节点
     $('.slide-li:last-child').clone().addClass('clone').prependTo('#slides');
     $('.slide-li').eq(1).clone().addClass('clone').appendTo('#slides');
     
-    var slideNum = $('.slide-li').length,
-        width = 1920 * slideNum,
-        left = -1920,
-        index = 1,
-        windowWidth = window.innerWidth,
-        autoTimer = null;
-    var imgWidth = $('.slide-li').width();
-    $('.slide-li img').css('marginLeft', -(imgWidth - windowWidth) / 2 + 'px');
-    $('#slides').css({'width': width + 'px', 'left': left + 'px'});
+    var imgWidth = 0, // 图片宽度
+        slideNum = $('.slide-li').length, // 滚动图片数，包括了附属节点
+        slidesWidth = 1920 * slideNum, // slide的总宽度
+        left = 0, //slide的左偏移量
+        index = 1, // 当前图片的索引
+        windowWidth = window.innerWidth, // 浏览器窗口宽度
+        buttons = $('.buttons li'), // 底部切换按钮
+        delayTimer = null, // 底部按钮延迟切换定时器
+        autoTimer = null; // 自动切换的定时器
+    $('.slide-li img').on('load', function() { // 图片宽度要在图片加载完成后才能获取，由于load事件的异步性，和imgWidth有关的语句都要写在load事件的回调函数里
+        imgWidth = $(this).width();
+        left = -imgWidth;
+        if (imgWidth > windowWidth) {
+            $('.slide-li').css('width', imgWidth + 'px');
+            $('.slide-li img').css('marginLeft', -(imgWidth - windowWidth) / 2 + 'px');
+        }
+        $('#slides').css({'width': slidesWidth + 'px', 'left': left + 'px'});
+    })
+    
     // 左右箭头切换
     $('.next').on('click', function(e) {
-        left-=1920;
-        $('#slides').animate({left: left + 'px'}, 800, function() {
+        left -= imgWidth;
+        $('#slides').stop(true, true).animate({left: left + 'px'}, 800, function() {
             index += 1;
             if (index > slideNum - 2) {
                 // 这里必须把css的left值复位的语句放在animate方法的complete回调函数里，否则由于异步性，会先执行-1920px的语句，然后800ms后执行完毕animate方法，又把left值改为-9600px，无法实现无缝滚动
-                $('#slides').css('left', -1920 + 'px');
+                $('#slides').css('left', -imgWidth + 'px');
                 index = 1;
-                left = -1920;
+                left = -imgWidth;
             }
+            $('.buttons li').eq(index - 1).addClass('on').siblings().removeClass('on');
         });
     });
     $('.prev').on('click', function(e) {
-        left += 1920;
-        $('#slides').animate({left: left + 'px'}, 800, function() {
+        left += imgWidth;
+        $('#slides').stop(true, true).animate({left: left + 'px'}, 800, function() {
             index -= 1;
             if (index < 1) {
                 // 这里必须把css的left值复位的语句放在animate方法的complete回调函数里，否则由于异步性，会先执行-1920px的语句，然后800ms后执行完毕animate方法，又把left值改为-9600px，无法实现无缝滚动
-                $('#slides').css('left', -1920*4 + 'px');
-                index = 4;
-                left = -1920*4;
+                $('#slides').css('left', -imgWidth*(slideNum - 2) + 'px');
+                index = (slideNum - 2);
+                left = -imgWidth*(slideNum - 2);
             }
+            buttons.eq(index - 1).addClass('on').siblings().removeClass('on');
         })
-        
+    });
+    // 下方圆按钮切换
+    buttons.on('mouseenter', function(e) {
+        var buttonIndex = buttons.index($(this)) + 1;
+        if (delayTimer) {
+            clearTimeout(delayTimer);
+            // delayTimer = null;
+        }
+        delayTimer = setTimeout(function() {
+            $('#slides').animate({left: -imgWidth * buttonIndex + 'px'}, 800, function() {
+                buttons.eq(buttonIndex - 1).addClass('on').siblings().removeClass('on');
+            });
+            delayTimer = null;
+        }, 400);
+    });
+    // 自动切换
+    if (autoTimer) {
+        clearInterval(autoTimer);
+        autoTimer = null;
+    }
+    autoTimer = setInterval(function() {
+        left -= imgWidth;
+        $('#slides').stop(true, true).animate({left: left + 'px'}, 800, function() {
+            index += 1;
+            if (index > slideNum - 2) {
+                // 这里必须把css的left值复位的语句放在animate方法的complete回调函数里，否则由于异步性，会先执行-1920px的语句，然后800ms后执行完毕animate方法，又把left值改为-9600px，无法实现无缝滚动
+                $('#slides').css('left', -imgWidth + 'px');
+                index = 1;
+                left = -imgWidth;
+            }
+            $('.buttons li').eq(index - 1).addClass('on').siblings().removeClass('on');
+        });
+    }, 2800);
+    $('#slideCon').hover(function() {
+        /* Stuff to do when the mouse enters the element */
+        clearInterval(autoTimer);
+        autoTimer = null;
+    }, function() {
+        /* Stuff to do when the mouse leaves the element */
+        autoTimer = setInterval(function() {
+            left -= imgWidth;
+            $('#slides').stop(true, true).animate({left: left + 'px'}, 800, function() {
+                index += 1;
+                if (index > slideNum - 2) {
+                    // 这里必须把css的left值复位的语句放在animate方法的complete回调函数里，否则由于异步性，会先执行-1920px的语句，然后800ms后执行完毕animate方法，又把left值改为-9600px，无法实现无缝滚动
+                    $('#slides').css('left', -imgWidth + 'px');
+                    index = 1;
+                    left = -imgWidth;
+                }
+                $('.buttons li').eq(index - 1).addClass('on').siblings().removeClass('on');
+            });
+        }, 2800);
     });
     $(window).on('resize', function() {
         windowWidth = window.innerWidth;
